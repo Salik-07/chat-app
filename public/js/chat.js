8f0173fb7c3e38d33ec22ecfb2bd817b0d76864f
Salik-07 <salikhussain41@gmail.com>
2024-06-04 00:50:48 +0500

    Sharing location in real time

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,6 +2,7 @@ const socket = io();
 
 // Elements
 const $messageForm = document.querySelector("#message-form");
+const $sendLocationButton = document.querySelector("#send-location");
 
 socket.on("message", (message) => {
   console.log(message);
@@ -13,4 +14,16 @@ $messageForm.addEventListener("submit", (e) => {
   const message = e.target.elements.message.value;
 
   socket.emit("sendMessage", message);
+});
+
+$sendLocationButton.addEventListener("click", () => {
+  if (!navigator.geolocation) {
+    return alert("Geolocation is not supported by your browser");
+  }
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    const { latitude, longitude } = position.coords;
+
+    socket.emit("sendLocation", { latitude, longitude });
+  });
 });
