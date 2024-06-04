29b8c0b7363d5de44bfc036ba4c8d5432236168f
Salik-07 <salikhussain41@gmail.com>
2024-06-05 00:05:44 +0500

    Set event acknowledgements and render messages dynamically

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,18 +2,38 @@ const socket = io();
 
 // Elements
 const $messageForm = document.querySelector("#message-form");
+const $messageFormInput = $messageForm.querySelector("input");
+const $messageFormButton = $messageForm.querySelector("button");
 const $sendLocationButton = document.querySelector("#send-location");
+const $messages = document.querySelector("#messages");
+
+// Templates
+const messageTemplate = document.querySelector("#message-template").innerHTML;
 
 socket.on("message", (message) => {
-  console.log(message);
+  const html = Mustache.render(messageTemplate, {
+    message: message,
+  });
+
+  $messages.insertAdjacentHTML("beforeend", html);
 });
 
 $messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
+  $messageFormButton.setAttribute("disabled", "disabled");
+
   const message = e.target.elements.message.value;
 
-  socket.emit("sendMessage", message);
+  socket.emit("sendMessage", message, (error) => {
+    $messageFormButton.removeAttribute("disabled");
+    $messageFormInput.value = "";
+    $messageFormInput.focus();
+
+    if (error) {
+      return console.log(error);
+    }
+  });
 });
 
 $sendLocationButton.addEventListener("click", () => {
@@ -21,9 +41,13 @@ $sendLocationButton.addEventListener("click", () => {
     return alert("Geolocation is not supported by your browser");
   }
 
+  $sendLocationButton.setAttribute("disabled", "disabled");
+
   navigator.geolocation.getCurrentPosition((position) => {
     const { latitude, longitude } = position.coords;
 
-    socket.emit("sendLocation", { latitude, longitude });
+    socket.emit("sendLocation", { latitude, longitude }, () => {
+      $sendLocationButton.removeAttribute("disabled");
+    });
   });
 });
