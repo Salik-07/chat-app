238cf56bb1813d1f0ce43cd20684c68e408527fc
Salik-07 <salikhussain41@gmail.com>
2024-06-05 00:32:04 +0500

    Render location messages

--- public/js/chat.js
+++ public/js/chat.js
@@ -9,10 +9,21 @@ const $messages = document.querySelector("#messages");
 
 // Templates
 const messageTemplate = document.querySelector("#message-template").innerHTML;
+const locationMessageTemplate = document.querySelector(
+  "#location-message-template"
+).innerHTML;
 
 socket.on("message", (message) => {
   const html = Mustache.render(messageTemplate, {
     message: message,
+  });
+
+  $messages.insertAdjacentHTML("beforeend", html);
+});
+
+socket.on("locationMessage", (url) => {
+  const html = Mustache.render(locationMessageTemplate, {
+    url: url,
   });
 
   $messages.insertAdjacentHTML("beforeend", html);
